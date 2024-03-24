611e3b90927ed0361b94dc2e99822e2557b0dcc2
jon429r <jonathanday088@gmail.com>
2024-03-23 23:14:19 -0400

    Commit

--- Algorithms/Exersize-5.py
+++ Algorithms/Exersize-5.py
@@ -1,8 +1,10 @@
 """# 18, 19, 21, 22, 25, 26, 27, 28, 32, 34, 35"""
+# Best 8 out of 11
+# Completion status: 7/11
 
 # 18
 
-# 19
+# 19: complete
 
 """
 Use Quicksort (Algorithm 2.6) to sort the following list. Show the actions step by step.
@@ -23,24 +25,52 @@ final sorted list: [9, 12, 24, 56, 123, 150, 189, 240]
 
 # 22
 
-# 25
+# 25: complete
 
-# 26
+"""
+The number of additions performed by algorithm 1.4 can be 
+reduced by modifying the algorithm to not add the elements
+of the matrix that are multiplied by 0. The number of additions can be
+reduced by n^2 - n, where n is the number of elements in the
+matrix.
+"""
 
-# 27
+# 26: Complete
+
+"""
+In Example 2.4, we gave Strassen’s product of two 2 × 2 
+matrices. Verify the correctness of this product.
+"""
+
+"""
+m1 = (a11 + a22)(b11 + b22)
+m2 = (a21 + a22)b11
+m3 = a11(b12 - b22)
+m4 = a22(b21 - b11)
+m5 = (a11 + a12)b22
+m6 = (a21 - a11)(b11 + b12)
+m7 = (a12 - a22)(b21 + b22)
+
+If A = [2 4; 1 3] and B = [5 7; 6 8]
+
+C = [(2*5 + 4*6) (2*7 + 4*8); (1*5 + 3*6) (1*7 + 3*8)]
+C = [34 46; 23 31]
+"""
+
+# 27: Complete
 
 """
 If its a 64 * 64 matrix, and the standard algorithm is N^3, the total number of multiplications are
 64^3 = 262,144
 """
 
-# 28
+# 28: Complete
 
 """
 The time complexity of Strassen's algorithm is N^log7, the total number of multiplications are
 64^log7 = 117,649
 """
-# 32
+# 32: Complete
 
 def divide(u, m):
     quotient = 0
@@ -55,7 +85,7 @@ print(divide(10, 5))
 """ This is a linear time complexity algorithm because the while loop runs until u is less than m.
 The time complexity is O(u/m) while is linear. """
 
-# 34
+# 34: Complete
 """
 Implement both Exchange Sort and Quicksort algorithms on your computer to sort a list of n elements. 
 Find the lower bound for n that justifies application of the Quicksort algorithm with its overhead.
